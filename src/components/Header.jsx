82abbd4af3392468b8a3f73534fd11726f0c79ab
Practicas-I-credit <practicas@i-credit.es>
2026-06-11 17:29:14 +0200

    feat: añadir logo y favicon SVG

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ export default function Header() {
   return (
     <header>
       <img src="/images/logo.svg" alt="HipotecaInSpain" id="header-logo" />
+      <span id="header-name">Hipoteca<span className="header-accent">InSpain</span></span>
     </header>
   )
 }
