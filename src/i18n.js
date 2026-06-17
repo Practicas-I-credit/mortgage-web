c2deb21161c347b3644b6f29a87bf4c3c7b7424b
Practicas-I-credit <practicas@i-credit.es>
2026-06-17 09:45:59 +0200

    feat: añadir traducción al francés — Home y Footer

--- src/i18n.js
+++ src/i18n.js
@@ -2,13 +2,15 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import en from './locales/en.json'
 import es from './locales/es.json'
+import fr from './locales/fr.json'
 
 i18n
   .use(initReactI18next)
   .init({
     resources: {
       en: { translation: en },
-      es: { translation: es }
+      es: { translation: es },
+      fr: { translation: fr }
     },
     lng: 'en',
     fallbackLng: 'en',
